b047dd9b37d2d0f071a9e1cda0eefe4c8ef2755d
ksy132336 <ksygiru@gmail.com>
2025-05-12 03:23:34 +0900

    JPA 프로그래밍

---
+++ src/week4/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+    private String Name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
