d5ae0d047fe50f31373b9c3abaaac0936c36670a
ksy132336 <ksygiru@gmail.com>
2025-05-18 19:29:39 +0900

    데이터베이스 h2->mysql로 변경

--- src/week4/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ src/week4/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,19 +17,28 @@ public class JpaMain {
 
         try {
             //Member findMember = em.find(Member.class,1L );
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//           for (Member member : result) {
+//               System.out.println("member.name = " + member.getName());
+//           }
 
-           for (Member member : result) {
-               System.out.println("member.name = " + member.getName());
-           }
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("member1");
+            System.out.println("회원저장" + member.getName());
+
+
+            em.persist(member);
 
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
